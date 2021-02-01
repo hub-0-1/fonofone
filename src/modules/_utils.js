d38df97aaa172aafeff4096229c85cf3ca548f55
Olivier Ross <olivier.ross@usherbrooke.ca>
2021-01-31 22:32:42 -0500

    notes pour implementation modules

--- src/modules/_utils.js
+++ src/modules/_utils.js
@@ -6,6 +6,21 @@ export default {
     "generique": Generique
   },
   methods: {
-    update_disposition: function (e) { this.$emit('update:disposition', e); }
+    update_disposition: function (e) { 
+      this.$emit('update:disposition', e); 
+    },
+    drag_start: function (e) {
+      // TODO garder en memoire x y initial
+    },
+    drag: function (e) {
+      // TODO comparer, faire bouger controlleur et mettre a jour valeurs
+    }
+  },
+  mounted: function () {
+    if(!this.$refs.controlleur) throw "Controlleur non-implemente";
+
+    let controlleur = this.$refs.controlleur;
+    //dragstart (mouse et touch)
+    //drag (mouse et touch)
   }
 }
--- src/modules/volume.js
+++ src/modules/volume.js
@@ -18,7 +18,7 @@ export default {
       <svg viewBox="0 0 100 100" preserveAspectRatio="none" style="width: 100%; height: 100%;">
         <rect x="5" width="90" y="5" height="90" rx="5" style="fill:green; fill-opacity:0.5;"/>
         <rect x="49" width="2" y="5" height="90" style="fill:green;"/>
-        <rect x="40" width="20" y="45" height="10" rx="5" style="fill:green; stroke:white; stroke-width:5; fill-opacity:0.5;"/>
+        <rect x="40" width="20" y="45" height="10" rx="5" style="fill:green; stroke:white; stroke-width:5; fill-opacity:0.5;" ref="controlleur"/>
       </svg>
       <input v-model.number="volume" @input="this.update" type="hidden" step="0.1">
     </generique>
