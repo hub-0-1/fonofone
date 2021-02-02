f57fd01e924b6602579ee3df78fd6d843874765e
Olivier Ross <olivier.ross@usherbrooke.ca>
2021-02-02 16:29:41 -0500

    manip selecteur

--- src/modules/selecteur.js
+++ src/modules/selecteur.js
@@ -1,5 +1,8 @@
 import Utils from "./_utils.js";
 
+const coordonnees_triangle = { hauteur: 100, largeur: 100 };
+const ratio_controlleur = 10;
+
 export default {
   mixins: [Utils],
   data: function () {
@@ -11,14 +14,14 @@ export default {
     },
     drag: function (e) {
       let coords = this.get_mouse_position(e);
-      this.$refs.controlleur.style.transform = `translate(${coords.x}%, ${coords.y}%)`;
+      this.$refs.controlleur.style.transform = `translate(${coords.x - 5}%, ${coords.y - 5}%)`; // TODO enlever les magic numbers
     }
   },
   template: `
     <generique class="generique" :module="$t('modules.selecteur')" :disposition="disposition" :modifiable="modifiable" @redispose="this.update_disposition">
       <svg viewBox="0 0 100 100" preserveAspectRatio="none" ref="canvas">
-        <polygon class="triangle" points="5,95 50,5 95,95"/>
-        <polygon class="controlleur" points="0,10 5,0 10,10" ref="controlleur"/>
+        <polygon class="triangle" points="0,${coordonnees_triangle.hauteur} ${coordonnees_triangle.largeur / 2},0 ${coordonnees_triangle.largeur},${coordonnees_triangle.hauteur}"/>
+        <polygon class="controlleur" points="0,${coordonnees_triangle.hauteur / ratio_controlleur} ${coordonnees_triangle.largeur / ratio_controlleur / 2},0 ${coordonnees_triangle.largeur / ratio_controlleur},${coordonnees_triangle.hauteur / ratio_controlleur}" ref="controlleur"/>
       </svg>
     </generique>
   `
