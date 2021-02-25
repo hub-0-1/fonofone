96032b356b7d4a6251f04e3eb62b538c10f5ff87
Olivier Ross <olivier.ross@usherbrooke.ca>
2021-02-24 21:54:03 -0500

    positions vitesse

--- src/modules/vitesse.js
+++ src/modules/vitesse.js
@@ -2,7 +2,7 @@ import Utils from "./_utils.js";
 import Magnet from "../images/icon-magnet.svg";
 
 const largeur_vitesse = 0.1;
-const nb_division = 8;
+const nb_division = 7;
 const width_c2 = 0.02;
 const width_c4 = 0.01;
 const width_c8 = 0.01;
@@ -37,13 +37,14 @@ export default {
     <generique :module="$t('modules.vitesse')" :disposition="disposition" :modifiable="modifiable && !is_dragging" @redispose="this.update_disposition">
       <svg viewBox="0 0 1 1" preserveAspectRatio="none" ref="canvas">
         <rect class="bg" x="0" width="1" y="0" height="1"/>
-        <rect class="coche c8" x="${0.125 - width_c8 / 2}" y="0.4" height="0.2" width="${width_c8}" />
-        <rect class="coche c4" x="0.245" y="0.2" height="0.6" width="0.01" />
-        <rect class="coche c8" x="${0.375 - width_c8 / 2}" y="0.4" height="0.2" width="${width_c8}" />
-        <rect class="coche c2" x="0.49" y="0" height="1" width="0.02" />
-        <rect class="coche c8" x="${0.625 - width_c8 / 2}" y="0.4" height="0.2" width="${width_c8}" />
-        <rect class="coche c4" x="0.745" y="0.2" height="0.6" width="0.01" />
-        <rect class="coche c8" x="${0.875 - width_c8 / 2}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(0 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(1 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(2 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(3 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(4 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(5 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(6 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
+        <rect class="coche c8" x="${(7 / nb_division) * (1 - largeur_vitesse) + (largeur_vitesse / 2) - (width_c8 / 2)}" y="0.4" height="0.2" width="${width_c8}" />
         <rect class="controlleur" :x="x" width="${largeur_vitesse}" y="0" height="1" rx="0.02" ref="controlleur"/>
       </svg>
 
